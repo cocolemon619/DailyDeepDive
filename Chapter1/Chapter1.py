30fbb0d89c1c98ea1f0f2505fc63a21f0eebb61e
cocolemon619 <torikuto.66@gmal.com>
2024-08-06 22:05:50 +0900

    1.5まで

--- Chapter1/Chapter1.py
+++ Chapter1/Chapter1.py
@@ -1,9 +1,55 @@
-# # 1.4.1
+# # 1.4
 # print("I'm hungry!")
 
-# man.py の実行
-from man import Man
+# # man.py の実行
+# from man import Man
 
-m = Man("David")
-m.hello()
-m.goodbye()
+# m = Man("David")
+# m.hello()
+# m.goodbye()
+
+# 1.5
+import numpy as np
+x = np.array([1.0 , 2.0 , 3.0])
+print(x)
+print(type(x))
+
+x = np.array([1.0 , 2.0 , 3.0])
+y = np.array([2.0 , 4.0 , 6.0])
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+x = np.array([1.0, 2.0, 3.0])
+print(x/2.0)
+
+A = np.array([[1,2],[3,4]])
+print(A)
+print(A.shape)
+print(A.dtype)
+
+B = np.array([[3,0],[0,6]])
+print(A + B)
+print(A * B)
+
+print(A * 10)
+
+A = np.array([[1,2],[3,4]])
+B = np.array([10,20])
+print(A * B)
+
+X = np.array([[51,55],[14,19],[0,4]])
+print(X)
+print(X[0])
+print(X[0][1])
+
+for row in X:
+    print(row)
+
+X = X.flatten()
+print(X)
+print(X[np.array([0,2,4])])
+
+print(X > 15)
+print(X[X>15])
