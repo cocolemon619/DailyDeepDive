96cf867601a8fbed227c3d7716628436b4410681
cocolemon619 <torikuto.66@gmal.com>
2024-08-09 00:48:09 +0900

    3.2.2までの実践で間違いに気づいたため、上げ直し

--- Chapter3/Chapter3.py
+++ Chapter3/Chapter3.py
@@ -7,13 +7,10 @@ def step_function(x):
     else:
         return 0
 
-def step_function2(x):
+def step_function_list(x): # 引数xはlist形式
     y = x > 0
     # return y.astype(int) がAttributeErrorで実行できなかったため、以下で実行
-    return int(y)
+    return y.astype(int) # list形式で与えると大丈夫だったけど、list形式で与えないとだめだった。
 
 print(step_function(3.4))
-print(step_function2(3.4))
-
-# print(step_function(np.array([3,4,3.3]))) # 
-print(step_function2(np.array([3,4,3.3])))
+print(step_function_list(np.array([3,4,3.3])))
